1ab4cd71c8486945d827f7a7a4aac2fa117ea00f
alex-ortan <13038064+alex-ortan@users.noreply.github.com>
2023-11-22 11:42:04 -0500

    update dictionary

--- default/config/fish/config.fish
+++ default/config/fish/config.fish
@@ -3,6 +3,6 @@ source ~/.config/fish/functions/aliases.fish
 fish_add_path -m ~/.local/bin
 
 if status is-interactive
-and not set -q TMUX
-    exec tmux
+    and not set -q TMUX
+    exec tmux new-session -A -D -s main
 end
